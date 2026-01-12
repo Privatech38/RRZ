00877f6e9b3734794c27f860992443d76c175a66
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-01-12 14:22:02 +0100

    Add Naloga 1 a and b

---
+++ vaja_4/naloga_1.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# capture_f1.jpg koti
+top_left = np.array([331, 321])
+top_right = np.array([1312, 313])
+bottom_left = np.array([414, 890])
+bottom_right = np.array([1234, 889])
+
+if __name__ == "__main__":
+    # a)
+    retval, mask = cv2.findHomography(np.array([top_left, top_right, bottom_left, bottom_right]),
+                                      np.array([[0,0], [400, 0], [0, 270], [400, 270]]))
+
+    original = cv2.imread('material/capture_f1.jpg', cv2.IMREAD_COLOR_RGB)
+    warped = cv2.warpPerspective(original, retval, (400, 270))
+
+    xx, yy = np.meshgrid(np.arange(0, 401, 10), np.arange(0, 271, 10))
+    work_grid_points = np.c_[xx.ravel(), yy.ravel()]
+
+    # Draw corners
+    plt.figure(figsize=(12.8, 9.6))
+    plt.subplot(1,2,2)
+    plt.imshow(warped)
+    plt.scatter(work_grid_points[:, 0], work_grid_points[:, 1], c='b', marker='.', s=10)
+
+    # b)
+    inverse_H = np.linalg.inv(retval)
+    homogeneous_work_grid = np.hstack((work_grid_points, np.ones((work_grid_points.shape[0], 1))))
+    original_work_grid = (inverse_H@homogeneous_work_grid.T).T
+    original_work_grid = original_work_grid[:, :2] / original_work_grid[:, [-1]]
+
+    plt.subplot(1,2,1)
+    plt.imshow(original)
+    plt.scatter(original_work_grid[:, 0], original_work_grid[:, 1], c='b', marker='.', s=3)
+    plt.show()
